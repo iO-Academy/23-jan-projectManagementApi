e712e3e4005bd8dfbd913016f15d1eb0dd5f73ef
barebarebears <nick.hackland@gmail.com>
2023-03-21 09:47:21 +0000

    project hydrator complete

--- src/Hydrators/ProjectHydrator.php
+++ src/Hydrators/ProjectHydrator.php
@@ -1,3 +1,17 @@
 <?php
 
 namespace ProjMange\Hydrators;
+
+use ProjMange\Entities\ProjectEntity;
+
+class ProjectHydrator
+{
+    public static function getAllProjects(PDO $db)
+    {
+        $stmt = $db->prepare("SELECT `id`, `name`, `client_id`, `deadline` FROM `projects`;");
+        $stmt->setFetchMode(PDO::FETCH_CLASS, ProjectEntity::class);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+}
+
