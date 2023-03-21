a0347b83293cf2ae5034b166040e353b1adb9072
Steph-ski <stephaniejohns57@gmail.com>
2023-03-21 11:18:27 +0000

    dock blocks added to object  methods

--- src/Services/DbConnService.php
+++ src/Services/DbConnService.php
@@ -6,11 +6,22 @@ class DbConnService
 {
     private $db;
 
+
+    /**
+     * Constructor method to create db connection when DbConnService object instansiated
+     *
+     */
     public function __construct()
     {
         $this->db = new PDO('mysql:host=db; dbname=project_manager', 'root', 'password');
     }
 
+
+    /**
+     * Retrieves the PDO connection for the DbConnService object
+     *
+     * @return $db object with PDO connection
+     */
     public function getConn(): PDO
     {
         return $this->db;
