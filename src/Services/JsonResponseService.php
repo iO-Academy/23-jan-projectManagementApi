d6b92d430950036c52d5828163d1025e767057cf
alreuben <ayannahreuben@gmail.com>
2023-03-21 11:14:03 +0000

    code review changes made

--- src/Services/JsonResponseService.php
+++ src/Services/JsonResponseService.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace ProjMange\Services;
+
 abstract class JsonResponseService
 {
+    /**
+     * Returns the inputted message and data as an array
+     *
+     * @param string $message
+     * @param array $data
+     * @return array with the message input and the data input
+     */
     public static function jsonResponse(string $message, array $data = []): array
     {
-        return ['data' => $data, 'message' => $message];
+        return ['message' => $message, 'data' => $data];
     }
 }
