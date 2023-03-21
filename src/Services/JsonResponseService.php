19d5d082f2a9444099d0a4986fcf4f70de978ddc
alreuben <ayannahreuben@gmail.com>
2023-03-21 10:52:29 +0000

    unti testing added

--- src/Services/JsonResponseService.php
+++ src/Services/JsonResponseService.php
@@ -1,26 +1,10 @@
 <?php
-//JsonResponseService
-//CreateJsonResponse
-//Input = data, message
 
-//To follow the structure of {
-//"message": "Successfully retrieved project",
-//"data": {
-
-class JsonResponseService
+namespace ProjMange\Services;
+abstract class JsonResponseService
 {
-    public function jsonResponse($data, $message string) //function that takes in the data and a message string
+    public static function jsonResponse(string $message, array $data = []): array
     {
-        $res={}
-        if (array_key_exists('$id',$data)) { //if id exists, then message will be successful
-            $message = 'Successfully retrieved project';
-            $res .= $message;
-            } else {
-    $message = 'Invalid client ID';
-    $res .= $message;
+        return ['data' => $data, 'message' => $message];
     }
-        return json_encode($res);
-    }
-
-
 }
