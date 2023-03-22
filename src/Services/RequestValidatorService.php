fafa22ba56d5a58214085adade6131acebc48ed4
barebarebears <nick.hackland@gmail.com>
2023-03-22 14:42:21 +0000

    removed part of the code

--- src/Services/RequestValidatorService.php
+++ src/Services/RequestValidatorService.php
@@ -7,16 +7,10 @@ abstract class RequestValidatorService
     public static function validateId(int $inputId, array $projects): bool
     {
         $validatedId = filter_var($inputId, FILTER_VALIDATE_INT);
-        if ($validatedId !== false && $validatedId < 1000000 && array_key_exists($inputId, $projects)) {
+        if ($validatedId !== false && $validatedId < 1000000) {
             return true;
         } else {
             return false;
         }
     }
 }
-
-
-
-
-//If the id exists in th db return project, else catch error Code: 400 BAD REQUEST
-//Content: {"message": "Invalid project ID", "data": []}
