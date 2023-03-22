be692a5837579d45e362747cb341185c80f0d331
barebarebears <nick.hackland@gmail.com>
2023-03-22 15:07:00 +0000

    changes made following code review

--- src/Services/RequestValidatorService.php
+++ src/Services/RequestValidatorService.php
@@ -4,7 +4,13 @@ namespace ProjMange\Services;
 
 abstract class RequestValidatorService
 {
-    public static function validateId(int $inputId, array $projects): bool
+
+    /**
+     * Validates the input id to make sure it exists, is a number and is not too big
+     * @param int $inputId
+     * @return bool
+     */
+    public static function validateId(int $inputId): bool
     {
         $validatedId = filter_var($inputId, FILTER_VALIDATE_INT);
         if ($validatedId !== false && $validatedId < 1000000) {
