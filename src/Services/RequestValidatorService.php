03a2bdf3ec3dd9f2509081e2000586d648bab044
barebarebears <nick.hackland@gmail.com>
2023-03-22 12:22:19 +0000

    small tweaks

--- src/Services/RequestValidatorService.php
+++ src/Services/RequestValidatorService.php
@@ -4,10 +4,10 @@ namespace ProjMange\Services;
 
 abstract class RequestValidatorService
 {
-    public static function validateId(int $id, array $projects): bool
+    public static function validateId(int $inputId, array $projects): bool
     {
-        $validatedId = filter_var($id, FILTER_VALIDATE_INT);
-        if ($validatedId !== false && $validatedId < 1000000 && array_key_exists($id, $projects)) {
+        $validatedId = filter_var($inputId, FILTER_VALIDATE_INT);
+        if ($validatedId !== false && $validatedId < 1000000 && array_key_exists($inputId, $projects)) {
             return true;
         } else {
             return false;
