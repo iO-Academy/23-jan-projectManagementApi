ca017222391bd0ea468f61bdfcaceafdff37b388
Steph-ski <stephaniejohns57@gmail.com>
2023-03-23 14:58:19 +0000

    unit testing for validator service

--- tests/Services/JsonResponseServiceTest.php
+++ tests/Services/JsonResponseServiceTest.php
@@ -3,29 +3,29 @@ use PHPUnit\Framework\TestCase;
 
 class JsonResponseServiceTest extends TestCase {
 
-    public function testSuccessJsonResponse()
+    public function testSuccessFormatJsonResponse()
     {
         $expected = ['message' => 'test', 'data' => ['id' => 1, 'name' => 'fake name']];
         $inputMessage = 'test';
         $inputData = ['id' => 1, 'name' => 'fake name'];
-        $result = \ProjMange\Services\JsonResponseService::jsonResponse($inputMessage, $inputData);
+        $result = \ProjMange\Services\JsonResponseService::formatJsonResponse($inputMessage, $inputData);
         $this->assertEquals($expected, $result);
     }
 
-    public function testFailureJsonResponse()
+    public function testFailureFormatJsonResponse()
     {
         $expected = ['message' => 'test', 'data' => []];
         $inputMessage = 'test';
-        $result = \ProjMange\Services\JsonResponseService::jsonResponse($inputMessage);
+        $result = \ProjMange\Services\JsonResponseService::formatJsonResponse($inputMessage);
         $this->assertEquals($expected, $result);
     }
 
-    public function testMalformedJsonResponse()
+    public function testMalformedFormatJsonResponse()
     {
         $this->expectException(TypeError::class);
         $inputMessage = [];
         $inputData = [];
-        $case = \ProjMange\Services\JsonResponseService::jsonResponse($inputData, $inputMessage);
+        $case = \ProjMange\Services\JsonResponseService::formatJsonResponse($inputData, $inputMessage);
     }
 }
 
